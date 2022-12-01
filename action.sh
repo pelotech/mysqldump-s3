1aa8aaba8b59a13a29b8c142a851dad9cb0b2684
Joachim Hill-Grannec <joachim.hill@gmail.com>
2022-11-30 20:08:13 -0600

    workflow to publish the container as well as initial script

--- action.sh
+++ action.sh
@@ -1,3 +1,48 @@
 #!/usr/bin/env bash
 
-echo "running command"
+########################################
+# Import/export SQL dumps via S3.
+# Required env vars:
+# * ACTION - either 'import' or 'export'
+# * DB_HOST
+# * DB_NAME
+# * DB_USER
+# * DB_PASS
+# * BUCKET
+# Optional when ACTION=export:
+# * OBJECT_NAME - default {ISO date}.sql
+########################################
+
+import () {
+  echo "Importing $OBJECT_NAME from $BUCKET to $DB_HOST"
+  aws s3 cp $BUCKET/$OBJECT_NAME $OBJECT_NAME
+  mysql $DB_NAME --host=$DB_HOST --user=$DB_USER --password=$DB_PASS < $OBJECT_NAME
+}
+
+export () {
+  if [[ $OBJECT_NAME = "" ]]; then
+    OBJECT_NAME=$(date -Iseconds).sql
+  fi
+
+  echo "Dumping DB from $DB_HOST"
+  mysqldump $DB_NAME \
+    --host=$DB_HOST \
+    --user=$DB_USER \
+    --password=$DB_PASS \
+    --result-file=$OBJECT_NAME \
+    --add-drop-table \
+    --verbose
+
+  echo "Backing up to $BUCKET as $OBJECT_NAME"
+  aws s3 cp $OBJECT_NAME $BUCKET/$OBJECT_NAME
+}
+
+
+if [[ $ACTION = "import" ]]; then
+  import
+elif [[ $ACTION = "export" ]]; then
+  export
+else
+  echo "ACTION must be either 'import' or 'export'"
+  exit 1
+fi
